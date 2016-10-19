9d2f09efb0db84c20ba63903ce681053cbc9c4f2
Kedar <kedar.gn20@gmail.com>
2016-10-19 01:45:00 -0400

    forecast decision new repo

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,3 @@
 echo 'killing existing flask process if any' >> /var/log/flask-before.log
-ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
+ps -ef | grep ForecastDecision | grep -v grep | awk '{print $2}' | xargs kill >> /var/log/flask-before.log
 sleep 5
--- scripts/install.sh
+++ scripts/install.sh
@@ -10,14 +10,3 @@ pip install Flask >> /var/log/sga-npcomplete-forecast-decision-install.log
 #echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
 export FLASK_APP=forecast_decision.py
 flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-forecast-decision-server.log 2>&1 &
-
-cd '/home/ec2-user/Forecast'
-echo 'Activating virtualenv for Forecast Microservice' >> /var/log/sga-npcomplete-forecast-install.log
-pip install virtualenv >> /var/log/sga-npcomplete-forecast-install.log
-virtualenv venv >> /var/log/sga-npcomplete-forecast-install.log
-. venv/bin/activate >> /var/log/sga-npcomplete-forecast-install.log
-pip install requests
-pip install Flask >> /var/log/sga-npcomplete-forecast-install.log
-#echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
-export FLASK_APP=forecast.py
-flask run --host=0.0.0.0 --port=64000 >> /var/log/sga-npcomplete-forecast-server.log 2>&1 &
