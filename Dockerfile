448bda04a62dc8a37bc9b2be1ec119a8f521c220
Kedar <kedar.gn20@gmail.com>
2016-10-20 01:59:14 -0400

    dockerify forecast detector

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:12.04
+
+RUN apt-get update
+RUN apt-get install -y python-dev python-pip
+ 
+ADD ./ForecastDecision/ ./
+RUN pip install virtualenv
+RUN virtualenv venv
+RUN . venv/bin/activate
+RUN pip install requests
+RUN pip install flask 
+ENV FLASK_APP=forecast_decision.py
+EXPOSE 65000
+CMD flask run --host=0.0.0.0 --port=65000
--- ForecastDecision/forecast_decision.py
+++ ForecastDecision/forecast_decision.py
@@ -18,18 +18,10 @@ def forecast_decision():
 
   headers = {'Content-Type': 'application/json'}
 
-  # r = requests.post("http://ec2-54-69-92-137.us-west-2.compute.amazonaws.com:8080/SG_MICROSERVICE_REGISTRY/gateway/message/saveData", data=json.dumps(result), headers=headers)
-  # print r.status_code
-
-
-  # for key in request.values:
-  # 	print "%s %s"%(key, request.values[key])
-  # connection = httplib.HTTPConnection("ec2-54-69-92-137.us-west-2.compute.amazonaws.com:8080");
-  # connection.request("POST", "/gateway/message/saveData", request.data)
-
+  
   if(random_no%2==0):
     parsed_json = {'result':'yes'}
-    connection = requests.post("http://0.0.0.0:64000/forecast/json", data=json.dumps(result));
+    connection = requests.post("http://ec2-54-69-92-137.us-west-2.compute.amazonaws.com:64000/forecast/json", data=json.dumps(result));
     print connection
     response_json = ast.literal_eval(connection.text)
     result["text"] = "Forecast Initiated"	  
