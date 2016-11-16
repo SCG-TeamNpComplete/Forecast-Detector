a2e0a3546f6495ade76754923037698ba1bfd0e5
Kedar <kedar.gn20@gmail.com>
2016-11-15 22:11:28 -0500

    replicate forecast detector #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,16 @@
 echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/forecastdetectortest
-sudo docker run -d -p 65000:65000 --name forecastdetectortest $(sudo docker images | grep kedargn/forecastdetectortest | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/forecastdetector
+
+sudo docker ps -a | grep 'forecastdetector1' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+sudo docker ps -a | grep 'forecastdetector1' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker run -d -p 65000:65000 --name forecastdetector1 $(sudo docker images | grep kedargn/forecastdetector | awk '{print $3}') >> ./log.txt
+
+
+sleep 2
+
+
+sudo docker ps -a | grep 'forecastdetector2' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+sudo docker ps -a | grep 'forecastdetector2' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker run -d -p 65001:65001 --name forecastdetector2 $(sudo docker images | grep kedargn/forecastdetector | awk '{print $3}') >> ./log.txt
