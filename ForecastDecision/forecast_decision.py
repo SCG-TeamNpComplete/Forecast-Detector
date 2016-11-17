b9b2a45729347e436a59ade5a2b6c3ef613b4b51
Soumya Achar <soumya@onlab.us>
2016-11-17 17:03:05 -0500

    #3 Register microservice with zookeeper server

--- ForecastDecision/forecast_decision.py
+++ ForecastDecision/forecast_decision.py
@@ -18,15 +18,6 @@ def forecast_decision():
 
   headers = {'Content-Type': 'application/json'}
 
-  # r = requests.post("http://ec2-54-69-92-137.us-west-2.compute.amazonaws.com:8080/SG_MICROSERVICE_REGISTRY/gateway/message/saveData", data=json.dumps(result), headers=headers)
-  # print r.status_code
-
-
-  # for key in request.values:
-  #   print "%s %s"%(key, request.values[key])
-  # connection = httplib.HTTPConnection("ec2-54-69-92-137.us-west-2.compute.amazonaws.com:8080");
-  # connection.request("POST", "/gateway/message/saveData", request.data)
-
   if(random_no%2==0):
     parsed_json = {'result':'yes'}
     connection = requests.post("http://ec2-35-160-137-157.us-west-2.compute.amazonaws.com:64000/forecast/json", data=json.dumps(result));
@@ -40,3 +31,41 @@ def forecast_decision():
     result["text"] = "Forecast Not Initiated"
     r = requests.post("http://ec2-35-160-137-157.us-west-2.compute.amazonaws.com:8080/SG_MICROSERVICE_REGISTRY/gateway/message/saveData", data=json.dumps(result), headers=headers)
     return jsonify({'result':'no'})
+
+def createConnection():
+  zk = KazooClient(hosts='127.0.0.1:2181')
+  zk.start()
+  zk.add_listener(my_listener)
+  print "tryin to create node"
+  if zk.exists("/load-balancing-example"):
+    print "----> node already exists"
+  else:
+    #Change this to refect dynamic ip and path
+    zk.create("/my/favorite/node3",b"http://12.22.33.22:1209",ephemeral=True,makepath=True)
+
+def tearDown():
+  zk.stop()
+
+def my_listener(state):
+  if state == KazooState.LOST:
+    # Register somewhere that the session was lost
+    zk = KazooClient(hosts='127.0.0.1:2181')
+    zk.start()
+
+  elif state == KazooState.SUSPENDED:
+    # Handle being disconnected from Zookeeper
+    print "connection suspended"
+  else:
+    # Handle being connected/reconnected to Zookeeper
+    print "connection error"
+
+#@app.before_first_request
+def connect():
+  try:
+    createConnection()
+  except KazooException as e:
+    print e.__doc__
+    print "error "+e.message
+  logging.basicConfig()
+
+connect()
