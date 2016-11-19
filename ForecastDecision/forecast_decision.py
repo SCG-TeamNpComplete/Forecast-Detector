0f1c3610d6d3f757394ef1f72d6e6ec64f65414e
Soumya Achar <soumya@onlab.us>
2016-11-18 23:59:40 -0500

    #7 Connect to ServiceGateway

--- ForecastDecision/forecast_decision.py
+++ ForecastDecision/forecast_decision.py
@@ -3,9 +3,17 @@ import httplib
 import requests
 import ast, random
 import json
+from kazoo.client import KazooClient
+from kazoo.client import KazooState
+from kazoo.exceptions import KazooException
+import logging
+import uuid
+import calendar, datetime, time
+from datetime import datetime
 
 app = Flask(__name__)
-
+ec2Ip = requests.get("http://checkip.amazonaws.com/").text.split("\n")[0]
+#ec2Ip = "127.0.0.1"
 @app.route('/forecast_decision/json', methods=['POST'])
 def forecast_decision():
   service_id = 5
@@ -15,7 +23,7 @@ def forecast_decision():
   result["serviceId"] = "ForecastDecision"
   headers = {'Content-Type': 'application/json'}
 
-  forecastUrl = requests.get("http://ec2-35-160-137-157.us-west-2.compute.amazonaws.com:9999/TeamNpComplete/Forecast")
+  forecastUrl = requests.get("http://ec2-35-160-137-157.us-west-2.compute.amazonaws.com:11000/servicegateway/forecast")
 
   if(random_no%2==0):
     parsed_json = {'result':'yes'}
@@ -31,24 +39,28 @@ def forecast_decision():
     r = requests.post("http://ec2-35-160-137-157.us-west-2.compute.amazonaws.com:8080/SG_MICROSERVICE_REGISTRY/gateway/message/saveData", data=json.dumps(result), headers=headers)
     return jsonify({'result':'no'})
 
+
 def createConnection():
-  zk = KazooClient(hosts='127.0.0.1:2181')
+  global ec2Ip
+  id = str(uuid.uuid4())
+  hostIp = ec2Ip+":2181"
+  zk = KazooClient(hosts=hostIp)
   zk.start()
   zk.add_listener(my_listener)
+  path = "http://"+ec2Ip+":65000/forecast_decision/json"
   print "tryin to create node"
-  if zk.exists("/load-balancing-example"):
-    print "----> node already exists"
-  else:
-    #Change this to refect dynamic ip and path
-    zk.create("/my/favorite/node3",b"http://12.22.33.22:1209",ephemeral=True,makepath=True)
+  #zk.create("/load-balancing-example/forecast",hostIp,ephemeral=True,makepath=True)
+  zk.create("/load-balancing-example/forecastDetector/"+id, json.dumps({'name': 'forecastDetector', 'id': id, 'address': ec2Ip, 'port': 65000,'sslPort': None, 'payload': None,'registrationTimeUTC': (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds(),'serviceType': 'DYNAMIC',"uriSpec": {"parts": [{"value": path, "variable": False}]}}, ensure_ascii=True).encode(),ephemeral=True,makepath=True)
 
 def tearDown():
   zk.stop()
 
 def my_listener(state):
+  global ec2Ip
   if state == KazooState.LOST:
     # Register somewhere that the session was lost
-    zk = KazooClient(hosts='127.0.0.1:2181')
+    hostIp = ec2Ip+":2181"
+    zk = KazooClient(hosts=hostIp)
     zk.start()
 
   elif state == KazooState.SUSPENDED:
@@ -68,3 +80,4 @@ def connect():
   logging.basicConfig()
 
 connect()
+
